bc55cfbf7e6836f2615b292f5d5a3874ced3bb7c
Matheus Raposo <matheus_raposo@hotmail.com>
2023-09-28 07:48:56 -0300

    move statements df to factory

--- tests/pdfs_test.py
+++ tests/pdfs_test.py
@@ -2,6 +2,8 @@ import os
 
 import pandas as pd
 
+from tests.support import factory
+
 from central_balancos_py.src.pdfs import filter_cnpjs, filter_types
 
 
@@ -9,25 +11,7 @@ def test_statement_read_worksheet():
     worksheet_path = os.path.join(os.getcwd(), 'tests', 'data', 'demonstracoes.xlsx')
     statements_sheet_name = 'demonstracoes'
     saved_df = filter_cnpjs(worksheet_path, statements_sheet_name)
-
-    expected_df = pd.DataFrame({
-        'nomeParticipante': ['ITATIAIA INVESTIMENTOS IMOBILIARIOS E PARTICIPACOES S.A.',
-                             'ITATIAIA INVESTIMENTOS IMOBILIARIOS E PARTICIPACOES S.A.',
-                             'APPLE'],
-        'tipoDemonstracao': ['Demonstrações Contábeis Completas (DCC)',
-                             'Balanço Patrimonial (BP)',
-                             'Demonstração do Resultado do Exercício (DRE)'],
-        'dataPublicacao': ['2019-11-20T22:55:55.627', '2023-06-21T11:24:32.34', '2022-11-20T22:55:55.627'],
-        'cnpj': ['13385440000156', '13385440000156', '12345670000890'],
-        'status': ['Publicado', 'Publicado', 'Publicado'],
-        'dataFim': ['2018-12-31T00:00:00', '2022-12-31T00:00:00', '2022-12-31T00:00:00'],
-        'pdf': [
-            'https://centraldebalancos.estaleiro.serpro.gov.br/centralbalancos/servicesapi/api/Demonstracao/pdf/3003',
-            'https://centraldebalancos.estaleiro.serpro.gov.br/centralbalancos/servicesapi/api/Demonstracao/pdf/77820',
-            'https://centraldebalancos.estaleiro.serpro.gov.br/centralbalancos/servicesapi/api/Demonstracao/pdf/0001'
-        ]
-    })
-    expected_df['cnpj'] = expected_df['cnpj'].astype('string')
+    expected_df = factory.statements_df()
 
     assert saved_df.equals(expected_df)
 
@@ -57,24 +41,7 @@ def test_filter_cnpjs():
 
 
 def test_filter_types():
-    statements = pd.DataFrame({
-        'nomeParticipante': ['ITATIAIA INVESTIMENTOS IMOBILIARIOS E PARTICIPACOES S.A.',
-                             'ITATIAIA INVESTIMENTOS IMOBILIARIOS E PARTICIPACOES S.A.',
-                             'APPLE'],
-        'tipoDemonstracao': ['Demonstrações Contábeis Completas (DCC)',
-                             'Balanço Patrimonial (BP)',
-                             'Demonstração do Resultado do Exercício (DRE)'],
-        'dataPublicacao': ['2019-11-20T22:55:55.627', '2023-06-21T11:24:32.34', '2022-11-20T22:55:55.627'],
-        'cnpj': ['13385440000156', '13385440000156', '12345670000890'],
-        'status': ['Publicado', 'Publicado', 'Publicado'],
-        'dataFim': ['2018-12-31T00:00:00', '2022-12-31T00:00:00', '2022-12-31T00:00:00'],
-        'pdf': [
-            'https://centraldebalancos.estaleiro.serpro.gov.br/centralbalancos/servicesapi/api/Demonstracao/pdf/3003',
-            'https://centraldebalancos.estaleiro.serpro.gov.br/centralbalancos/servicesapi/api/Demonstracao/pdf/77820',
-            'https://centraldebalancos.estaleiro.serpro.gov.br/centralbalancos/servicesapi/api/Demonstracao/pdf/0001'
-        ]
-    })
-    statements['cnpj'] = statements['cnpj'].astype('string')
+    statements = factory.statements_df()
 
     assert statements.equals(filter_types(statements, ''))
 
--- tests/support/factory.py
+++ tests/support/factory.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def company():
     return {'id': 635, 'cnpj': '13385440000156', 'nome': 'ITATIAIA INVESTIMENTOS IMOBILIARIOS E PARTICIPACOES S.A.'}
 
@@ -26,6 +29,28 @@ def row(name=None, cnpj=None):
         'dataPublicacao': '2023-06-21T11:24:32.34',
         'pdf': 'https://centraldebalancos.estaleiro.serpro.gov.br/centralbalancos/servicesapi/api/Demonstracao/pdf/77820'
     }
+
+
+def statements_df():
+    expected_df = pd.DataFrame({
+        'nomeParticipante': ['ITATIAIA INVESTIMENTOS IMOBILIARIOS E PARTICIPACOES S.A.',
+                             'ITATIAIA INVESTIMENTOS IMOBILIARIOS E PARTICIPACOES S.A.',
+                             'APPLE'],
+        'tipoDemonstracao': ['Demonstrações Contábeis Completas (DCC)',
+                             'Balanço Patrimonial (BP)',
+                             'Demonstração do Resultado do Exercício (DRE)'],
+        'dataPublicacao': ['2019-11-20T22:55:55.627', '2023-06-21T11:24:32.34', '2022-11-20T22:55:55.627'],
+        'cnpj': ['13385440000156', '13385440000156', '12345670000890'],
+        'status': ['Publicado', 'Publicado', 'Publicado'],
+        'dataFim': ['2018-12-31T00:00:00', '2022-12-31T00:00:00', '2022-12-31T00:00:00'],
+        'pdf': [
+            'https://centraldebalancos.estaleiro.serpro.gov.br/centralbalancos/servicesapi/api/Demonstracao/pdf/3003',
+            'https://centraldebalancos.estaleiro.serpro.gov.br/centralbalancos/servicesapi/api/Demonstracao/pdf/77820',
+            'https://centraldebalancos.estaleiro.serpro.gov.br/centralbalancos/servicesapi/api/Demonstracao/pdf/0001'
+        ]
+    })
+    expected_df['cnpj'] = expected_df['cnpj'].astype('string')
+    return expected_df
 
 
 __ALL__ = ['company', 'statement', 'row']
