575fc990cf7e7e1849a88528ac9e3f89ae48f400
Matheus Raposo <matheus_raposo@hotmail.com>
2023-09-30 12:39:08 -0300

    parametrize tests

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,16 +1,19 @@
-import unittest
 from unittest.mock import patch
+
+import pytest
+
 from central_balancos_py.src import main
 
 
-class UserInputTests(unittest.TestCase):
-    @patch("central_balancos_py.src.main.input")
-    def test_prompt_statement_type(self, mock_input):
-        mock_input.return_value = '1'
-        self.assertEqual('Balanço Patrimonial (BP)', main.prompt_statement_type())
-
-        mock_input.return_value = '2'
-        self.assertEqual('Demonstração do Resultado do Exercício (DRE)', main.prompt_statement_type())
-
-        mock_input.return_value = None
-        self.assertEqual('', main.prompt_statement_type())
+@pytest.mark.parametrize(
+    "user_input, expected_result",
+    [
+        ('1', 'Balanço Patrimonial (BP)'),
+        ('2', 'Demonstração do Resultado do Exercício (DRE)'),
+        (None, ''),
+    ]
+)
+@patch("central_balancos_py.src.main.input")
+def test_prompt_statement_type(mock_input, user_input, expected_result):
+    mock_input.return_value = user_input
+    assert expected_result == main.prompt_statement_type()
